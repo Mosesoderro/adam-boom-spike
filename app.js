f92d7cf3dda4df173a715183e6540443ad35acdd
Mosesoderro <153086856+Mosesoderro@users.noreply.github.com>
2025-05-25 09:53:06 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const signal = document.getElementById('signal');
-  setInterval(() => {
-    const spike = Math.random() < 0.1;
-    signal.textContent = spike ? "🚨 Spike Incoming! Get Ready!" : "Monitoring...";
-  }, 10000);
-});
